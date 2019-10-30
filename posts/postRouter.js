f0d58c79f62740ca4a0a1e22cae4634af0d64f6c
frescocodes <hueydakotah@gmail.com>
2019-10-30 17:47:15 -0400

    feat: delete post by id added

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -10,7 +10,9 @@ const {
   } = postDb;
 
 router.get('/', (req, res) => {
-    get().then(posts => res.status(200).json(posts)).catch(error => res.status(500).json({ error: "Failed to get posts" }))
+    get()
+    .then(posts => res.status(200).json(posts))
+    .catch(error => res.status(500).json({ error: "Failed to get posts" }))
 });
 
 router.get('/:id', (req, res) => {
@@ -25,7 +27,14 @@ router.get('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+    const { id } = req.params
+    remove(id)
+    .then(post => {
+        !post
+        ? res.status(404).json({ message: "The post with the specified ID does not exist" })
+        : res.status(200).json({ message: `Post with id of ${id} deleted`})
+    .catch(error => res.status(500).json({ error: "The post could not be deleted" }))
+})
 });
 
 router.put('/:id', (req, res) => {
