62f37b0aeba8d46a066a9d5f3b01ea2baf8bf75e
frescocodes <hueydakotah@gmail.com>
2019-10-30 17:16:05 -0400

    feat: get post by id added

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -14,7 +14,14 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params
+    getById(id)
+    .then(post => {
+        !post
+        ? res.status(404).json({ message: "The post with the specified ID does not exist" })
+        : res.status(200).json(post)
+    })
+    .catch(error => res.status(500).json({ error: "Failed to get post" }))
 });
 
 router.delete('/:id', (req, res) => {
