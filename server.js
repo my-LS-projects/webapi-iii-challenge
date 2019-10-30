49e789cf319167432a96f5273c1fdb50ef16efbf
frescocodes <hueydakotah@gmail.com>
2019-10-30 17:02:00 -0400

    feat: logger middleware created

--- server.js
+++ server.js
@@ -9,13 +9,17 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-server.use('/api/users', userRouter)
-server.use('/api/posts', postRouter)
+server.use('/api/users', logger, userRouter)
+server.use('/api/posts', logger, postRouter)
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(new Date().toISOString());
+  console.log('HTTP Method: ', req.method)
+  console.log('Original URL: ', req.originalUrl)
+  
+  next()
 };
 
 module.exports = server;
